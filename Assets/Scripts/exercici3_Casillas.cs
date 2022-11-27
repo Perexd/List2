600a601c5e0ba9c550d2becf136b7cdd6c5824cc
Perexd <pereestade@gmail.com>
2022-11-27 19:41:37 +0100

    xdv

--- Assets/Scripts/exercici3_Casillas.cs
+++ Assets/Scripts/exercici3_Casillas.cs
@@ -6,34 +6,34 @@ public class exercici3_Casillas : MonoBehaviour
 {
     private int movement = 150; 
     private int boardLimit = 300;
-    private bool canMove = true; 
+    private bool mover = true; 
     private float speed = 170f; 
     private Vector3 nextPosition; 
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.RightArrow) && canMove)
+        if(Input.GetKeyDown(KeyCode.RightArrow) && mover)
         {
             if(transform.position.x != boardLimit) 
             {
                 nextPosition = new Vector3(transform.position.x + movement, transform.position.y, transform.position.z);
             }
         }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow) && canMove) 
+        else if(Input.GetKeyDown(KeyCode.LeftArrow) && mover) 
         {
             if (transform.position.x != -boardLimit)
             {
                 nextPosition = new Vector3(transform.position.x - movement, transform.position.y, transform.position.z);
             }
         }
-        else if(Input.GetKeyDown(KeyCode.UpArrow) && canMove)  
+        else if(Input.GetKeyDown(KeyCode.UpArrow) && mover)  
         {
             if (transform.position.y != boardLimit)
             {
                 nextPosition = new Vector3(transform.position.x, transform.position.y + movement, transform.position.z);
             }
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) && canMove) 
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && mover) 
         {
             if (transform.position.y != -boardLimit)
             {
@@ -47,13 +47,13 @@ public class exercici3_Casillas : MonoBehaviour
         }
         if (nextPosition == transform.position) 
         {
-            canMove = true;
+            mover = true;
             
         }
     }
     public void Moving()
     {
-        canMove = false; 
+        mover = false; 
 
         transform.position = Vector3.MoveTowards(transform.position, nextPosition, speed * Time.deltaTime); 
     }
